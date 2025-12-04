938d7d004d697791400933b7cb0eff4580336ac5
Vivekanand Kumar <vivekanandath04@gmail.com>
2025-12-04 21:44:53 +0530

    removed the all comment from backend also

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,40 +1,4 @@
-// import { ApiError } from "../utils/ApiError.js";
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import jwt from "jsonwebtoken";
-// import { newUser } from "../models/account.model.js";
 
-// export const verifyJWT = asyncHandler(async(req , _ , next) => {
-//   console.log("✅ verifyJWT middleware hit");
-
-//    try {
-//     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer " , "")
-    
-//     if (!token) { 
- 
-//      throw new ApiError(401 , "Unauthorized request")
-     
-//     }
- 
-//    const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
- 
-//    const user = await  newUser.findById(decodedToken?._id).select
-//    ("-password -refreshToken")
- 
-//    if (!user) {
- 
-//      throw new ApiError(401 , "Invalid Access Token")
-     
-//    }
- 
-//    req.user = user;
-//    next()
-//    } catch (error) {
-
-//     throw new ApiError(401, error?.message || "Invalid access token")
-    
-//    }
-
-// })
 
 
 
