10e9e0b7b92650cf776bfcbe2cbd5d291a964b69
Vivekanand Kumar <vivekanandath04@gmail.com>
2025-12-04 21:41:52 +0530

    removed the all comment from frontend

--- UserInterface/src/main.jsx
+++ UserInterface/src/main.jsx
@@ -1,14 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// // import App from './App.jsx'
-// import Routing from './routes/Routing'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//      <Routing/>
-//   </React.StrictMode>,
-// )
 
 
 
--- UserInterface/src/store/store.js
+++ UserInterface/src/store/store.js
@@ -1,13 +1,4 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import authReducer from './slice/authSlice.js';
 
-// const store = configureStore({
-//     reducer: {
-//         auth: authReducer,
-//     },
-// });
-
-// export default store;
     import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import storage from "redux-persist/lib/storage";
